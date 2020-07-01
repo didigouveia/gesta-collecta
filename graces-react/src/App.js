50fae39d612041abde32a3058b33f67e1d599118
didigouveia <dilara.gouveia@gmail.com>
2020-07-01 23:11:14 +0200

    todo app react example

--- graces-react/src/App.js
+++ graces-react/src/App.js
@@ -1,41 +1,37 @@
 import React, { Component } from 'react';
-import './App.css';
-import Ninjas from './Ninjas'
-import AddNinja from "./AddNinja";
+import Todos from './Todos'
+import AddTodo from './AddTodo'
 
 class App extends Component {
+  // root Component
   state = {
-    ninjas: [
-      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
-      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
-      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
+    todos: [
+      { id: 1, content: 'buy some milk' },
+      { id: 2, content: 'play mario kart' }
     ]
-  }
-
-  addNinja = (ninja) => {
-    ninja.id = Math.random();
-    const ninjas = [...this.state.ninjas, ninja];
+  }  
+  deleteTodo = (id) => {
+    // console.log(id);
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
     this.setState({
-      ninjas: ninjas
-    })
+      todos
+    })    
   }
-
-  deleteNinja = (id) => {
-    const ninjas = this.state.ninjas.filter(ninja => {
-      return ninja.id !== id
-    })
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    const todos = [...this.state.todos, todo];
     this.setState({
-      ninjas: ninjas
+      todos
     })
   }
-
   render() {
     return (
-      <div className="App">
-        <h1>My first React app!</h1>
-        <p>Welcome :)</p>
-        <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas} />
-        <AddNinja addNinja={this.addNinja} />
+      <div className="todo-app container">        
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
       </div>
     );
   }

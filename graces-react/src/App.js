088be07b5d327b118d786d187804205264235cb6
didigouveia <dilara.gouveia@gmail.com>
2020-07-02 16:44:44 +0200

    rest api with react example

--- graces-react/src/App.js
+++ graces-react/src/App.js
@@ -1,38 +1,27 @@
 import React, { Component } from 'react';
-import Todos from './Todos'
-import AddTodo from './AddTodo'
+import Navbar from './components/Navbar'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Post from './components/Post'
 
 class App extends Component {
   // root Component
-  state = {
-    todos: [
-      { id: 1, content: 'buy some milk' },
-      { id: 2, content: 'play mario kart' }
-    ]
-  }  
-  deleteTodo = (id) => {
-    // console.log(id);
-    const todos = this.state.todos.filter(todo => {
-      return todo.id !== id
-    });
-    this.setState({
-      todos
-    })    
-  }
-  addTodo = (todo) => {
-    todo.id = Math.random();
-    const todos = [...this.state.todos, todo];
-    this.setState({
-      todos
-    })
-  }
+  
   render() {
     return (
-      <div className="todo-app container">        
-        <h1 className="center blue-text">Todo's</h1>
-        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
-        <AddTodo addTodo={this.addTodo} />
-      </div>
+      <BrowserRouter>
+        <div className="App">        
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/:post_id' component={Post} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ graces-react/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid consequatur ratione, maiores blanditiis cumque laboriosam nihil iusto nulla ab, tempora aperiam magnam, architecto nesciunt. Tenetur repellendus nesciunt laboriosam illum eius.</p>
+        </div>
+    )
+}
+
+export default About
